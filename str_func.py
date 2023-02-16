2cfac41d01e826b34cbb45b003d4126f89ec0e56
Dmitry Zakhs <d.zakhs@gmail.com>
2023-02-16 23:04:38 +0300

    add docstring to 1 func, add new function

--- str_func.py
+++ str_func.py
@@ -1,2 +1,16 @@
 def str_upp(string):
+    """
+    Функция форматироватирования строки
+    :param string: строка
+    :return: строка в верхнем регистре
+    """
     return string.upper()
+
+
+def str_title(string):
+    """
+    Функция изменяет первые буквы слов в строке на верхний регистр
+    :param string: строка
+    :return: первые буквы слов в строке в верхем регистре
+    """
+    return string.title()
